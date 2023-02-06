0d754b1ee3da232d88688696f59c73570c8dc764
laxatzi <laxatzi@yahoo.com>
2023-02-06 16:11:23 +0200

    REFACTOR- change buttons to divs, ADD- event listener to card

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,10 +1,19 @@
 const cards = document.querySelectorAll('.js-button');
 const emojies = document.querySelectorAll('.js-emoji');
 
+// const targetDiv = document.getElementById("third");
+// const btn = document.getElementById("toggle");
+// btn.onclick = function () {
+//   if (targetDiv.style.display !== "none") {
+//     targetDiv.style.display = "none";
+//   } else {
+//     targetDiv.style.display = "block";
+//   }
+// };
+
 cards.forEach((card) => {
-    card.addEventListener(('click'), () => {
-        emojies.forEach((emoji) => {
-          console.log('hi');
-        });
-    });
+  card.addEventListener('click', function() {
+
+    card.style.backgroundColor = 'grey';
+  });
 });
